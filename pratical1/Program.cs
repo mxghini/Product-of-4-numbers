5fcf21cd7825b7bbc9687a587db776f365c7119f
Rahul <Rahul@LAPTOP-MMTP6HB3>
2022-10-28 11:25:00 +0530

    Add project files.

---
+++ pratical1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pratical1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int no1, no2, no3, no4, ans;
+            Console.Write("First Number ");
+            no1 = int.Parse(Console.ReadLine());
+            Console.Write("Second Number ");
+            no2 = int.Parse(Console.ReadLine());
+            Console.Write("Third Number ");
+            no3 = int.Parse(Console.ReadLine());
+            Console.Write("Fourth Number ");
+            no4 = int.Parse(Console.ReadLine());
+            ans = no1 * no2 * no3 * no4;
+            Console.WriteLine("Product of numbers are " + ans);
+            Console.ReadKey();
+
+        }
+    }
+}
